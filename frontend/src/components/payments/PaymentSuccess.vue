82a6cb8cb0f2f264ae390137308005f2481d87d0
nabin-8 <nabinacharya740@gmail.com>
2024-08-03 09:32:53 +0545

    payment success

---
+++ frontend/src/components/payments/PaymentSuccess.vue
@@ -0,0 +1,17 @@
+<script setup>
+
+</script>
+
+<template>
+<div class="row my-5">
+  <div class="col-md-6 mx-auto">
+    <div class="card">
+      <div class="card-body pl-5">
+          <h4 class="text-center">
+              Payment done successfully Thanks you.
+          </h4>
+      </div>
+    </div>
+  </div>
+</div>
+</template>
