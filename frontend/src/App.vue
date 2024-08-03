fe630687f8ea3032fef89d8eb20aa573b84cd0c2
nabin-8 <nabinacharya740@gmail.com>
2024-08-03 09:33:48 +0545

    product

---
+++ frontend/src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+
+import Header from "./components/layouts/Header.vue";
+</script>
+
+<template>
+<div class="container">
+  <Header></Header>
+  <router-view></router-view>
+</div>
+</template>
