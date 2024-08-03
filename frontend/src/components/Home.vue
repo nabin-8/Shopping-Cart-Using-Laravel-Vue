215cdb8f020376285222bfd3f40ec2c8aa830b51
nabin-8 <nabinacharya740@gmail.com>
2024-08-03 09:32:17 +0545

    home page

---
+++ frontend/src/components/Home.vue
@@ -0,0 +1,28 @@
+<script setup>
+import {onMounted, reactive} from "vue";
+  import axios from "axios";
+import ProductList from "../products/ProductList.vue";
+
+  const data=reactive({
+    products:[]
+  })
+
+  const fetchAllProducts=async ()=>{
+    try {
+      const response=await  axios.get('http://backend.test/api/products')
+      data.products=response.data.data
+    }catch (error){
+      console.log(error)
+    }
+  }
+
+  onMounted(()=>fetchAllProducts())
+</script>
+
+<template>
+<ProductList :products="data.products" />
+</template>
+
+<style scoped>
+
+</style>
