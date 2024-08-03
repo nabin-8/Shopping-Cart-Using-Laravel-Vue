aa100dd1267cc5bbe7bf502c1d38c3df6b716287
nabin-8 <nabinacharya740@gmail.com>
2024-08-03 09:33:22 +0545

    product list

---
+++ frontend/src/products/ProductList.vue
@@ -0,0 +1,18 @@
+<script setup>
+import ProductListItem from "./ProductListItem.vue";
+
+const props= defineProps({
+  products:{
+    type:Array,
+    required:true
+  }
+})
+</script>
+
+<template>
+<div class="row my-4">
+  <ProductListItem v-for="product in products" :key="product.id"
+                   :product="product"
+  />
+</div>
+</template>
